fdf5f3ab153c3574105e6a11bdf44e17bdb953b6
tishchenko <rtishchenko@corepartners.com>
2025-03-13 00:12:48 +0300

    Initialize app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import JsonUploader from "./components/JsonUploader";
+import SomeObjectsDataGrid from "./components/SomeObjectsDataGrid";
+
+const App = () => {
+  return (
+    <div>
+      <JsonUploader />
+      <SomeObjectsDataGrid />
+    </div>
+  );
+}
+
+export default App;
